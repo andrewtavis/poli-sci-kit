58e763943dec4c122b99461565cef5f1f1eaa5ee
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-03-21 12:46:14 +0100

    Update setup.py to use find_packages and src

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-from setuptools import find_namespace_packages
+from setuptools import find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup_args = dict(
     name="poli-sci-kit",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     version="0.1.2.3",
     author="Andrew Tavis McAllister",
     author_email="andrew.t.mcallister@gmail.com",
@@ -28,7 +30,6 @@ setup_args = dict(
     description="Political elections, appointment, analysis and visualization in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_namespace_packages(),
     license="new BSD",
     url="https://github.com/andrewtavis/poli-sci-kit",
 )
