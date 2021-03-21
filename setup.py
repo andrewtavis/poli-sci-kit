9f2c7bf00dfa0a596f78989cace4d0f3d01b5e99
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-03-21 12:38:26 +0100

    Update package structure and fix wheel/setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setup_args = dict(
     name="poli-sci-kit",
-    version="0.1.2.2",
+    version="0.1.2.3",
     author="Andrew Tavis McAllister",
     author_email="andrew.t.mcallister@gmail.com",
     classifiers=[
