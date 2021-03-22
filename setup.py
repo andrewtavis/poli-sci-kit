829516c1b539f1deee654a8d85a923f69afa768b
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-03-22 14:42:13 +0100

    Update to fix coloration bug in dispr_bar

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup_args = dict(
     name="poli-sci-kit",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    version="0.1.2.3",
+    version="0.1.2.4",
     author="Andrew Tavis McAllister",
     author_email="andrew.t.mcallister@gmail.com",
     classifiers=[
